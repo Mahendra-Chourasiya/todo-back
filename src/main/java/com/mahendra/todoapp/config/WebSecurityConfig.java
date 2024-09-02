83abccd413f322b9e2722635b006cdcd1d99f0c0
Mahendra-Chourasiya <96823758+Mahendra-Chourasiya@users.noreply.github.com>
2024-09-02 11:55:55 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/mahendra/todoapp/config/WebSecurityConfig.java
+++ src/main/java/com/mahendra/todoapp/config/WebSecurityConfig.java
@@ -71,7 +71,7 @@ public class WebSecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Adjust this to your frontend URL
+                        .allowedOrigins("https://todo-front-5txlxnlx1-mahendra-chourasiyas-projects.vercel.app","https://todo-back-production-eb45.up.railway.app") // Adjust this to your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
