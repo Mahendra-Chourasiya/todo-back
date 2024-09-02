c43cc77e655ed0ba31ab8a3fd918f724976e9095
Mahendra-Chourasiya <96823758+Mahendra-Chourasiya@users.noreply.github.com>
2024-09-02 13:27:25 +0530

    Update MailService.java

--- src/main/java/com/mahendra/todoapp/services/MailService.java
+++ src/main/java/com/mahendra/todoapp/services/MailService.java
@@ -3,7 +3,8 @@ package com.mahendra.todoapp.services;
 // import java.io.File;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
+// import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
